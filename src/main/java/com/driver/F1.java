54c792a5c2faadbc8319f50dae6c8591c256d184
rohitkishne <rohitkishne3@gmail.com>
2023-03-18 16:34:45 +0530

    Vehicle modified

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -7,7 +7,7 @@ public class F1 extends Car {
         super(name,4,4,6, isManual, "sedan", 4);
     }
 
-    public void accelerate(int rate){
+    public void accelerate(int rate) {
         int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
         newSpeed = rate + getCurrentSpeed();
         /**
@@ -20,38 +20,27 @@ public class F1 extends Car {
          * speed more than 250: gear 6
          */
 
-        if(newSpeed == 0) {
+        if (newSpeed == 0) {
             //Stop the car, set gear as 1
             changeGear(1);
         }
         //for all other cases, change the gear accordingly
 
-//        if(newSpeed > 0) {
-//            changeSpeed(newSpeed, getCurrentDirection());
-//        }
-        else if(newSpeed<=50)
-        {
-            changeGear(1);
-        }
-        else if(newSpeed<=100)
-        {
-            changeGear(2);
-        }
-        else if(newSpeed<=150)
-        {
-            changeGear(3);
-        }
-        else if(newSpeed<=200)
-        {
-            changeGear(4);
-        }
-        else if(newSpeed<=250)
-        {
-            changeGear(5);
-        }
-        else
-        {
-            changeGear(6);
+        if(newSpeed > 0) {
+            changeSpeed(newSpeed, getCurrentDirection());
+            if (newSpeed <= 50) {
+                changeGear(1);
+            } else if (newSpeed <= 100) {
+                changeGear(2);
+            } else if (newSpeed <= 150) {
+                changeGear(3);
+            } else if (newSpeed <= 200) {
+                changeGear(4);
+            } else if (newSpeed <= 250) {
+                changeGear(5);
+            } else if(newSpeed>250){
+                changeGear(6);
+            }
         }
     }
 
